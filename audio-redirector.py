b65b7f44aa732e6909a9bdda90bf8a3890d6dbc9
salledelavage <salledelavager@gmail.com>
2024-06-19 08:01:50 -0400

    testing ffmpeg

---
+++ audio-redirector.py
@@ -0,0 +1,26 @@
+import ffmpeg
+
+
+
+# stream = ffmpeg.formats('pulse')
+stream = ffmpeg.input('audio=default')
+stream = ffmpeg.output(stream, 'format=wav', '/tmp/pulse.wav')
+ffmpeg.run(stream)
+
+
+
+
+# FFmpeg command
+ffmpeg_command = [
+    'ffmpeg',
+    '-f', 'pulse',
+    '-i', 'audio=default', '/tmp/pulse.wav', # Change to your virtual audio device name
+    '-f', 'mpegts',
+    'udp://localhost:12345'
+]
+
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    exit()
