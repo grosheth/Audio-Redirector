7c66da4ccc9c1c0ac51c977571c51e4b2ad3f38e
salledelavage <salledelavager@gmail.com>
2024-07-16 17:40:32 -0400

    testing pyaufio

--- audio-redirector.py
+++ audio-redirector.py
@@ -1,29 +1,28 @@
-import ffmpeg
-import subprocess
+"""PyAudio Example: Record a few seconds of audio and save to a wave file."""
 
+import wave
+import sys
 
-subprocess.run(['aplay', '-l'])
+import pyaudio
 
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1 if sys.platform == 'darwin' else 2
+RATE = 44100
+RECORD_SECONDS = 5
 
-stream = ffmpeg.input('audio=USB')
-stream = ffmpeg.output(stream, 'formats=pulse', './pulse.wav')
-ffmpeg.run(stream)
+with wave.open('output.wav', 'wb') as wf:
+    p = pyaudio.PyAudio()
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
 
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
 
+    print('Recording...')
+    for _ in range(0, RATE // CHUNK * RECORD_SECONDS):
+        wf.writeframes(stream.read(CHUNK))
+    print('Done')
 
-
-
-# # FFmpeg command
-# ffmpeg_command = [
-#     'ffmpeg',
-#     '-f', 'pulse',
-#     '-i', 'audio=default', '/tmp/pulse.wav', # Change to your virtual audio device name
-#     '-f', 'mpegts',
-#     'udp://localhost:12345'
-# ]
-#
-# try:
-#     while True:
-#         pass
-# except KeyboardInterrupt:
-#     exit()
+    stream.close()
+    p.terminate()
--- shell.nix
+++ shell.nix
@@ -3,7 +3,9 @@ let
 in pkgs.mkShell {
   packages = [
     (pkgs.python311.withPackages (python-pkgs: [
-      python-pkgs.ffmpeg-python
+      # python-pkgs.ffmpeg-python
+      python-pkgs.pyaudio
+      # python-pkgs.wave
     ]))
   ];
 }
