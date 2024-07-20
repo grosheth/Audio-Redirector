63986b949a2e9f0934813a23887cb3b16e4cfab8
salledelavage <salledelavager@gmail.com>
2024-07-20 18:00:01 -0400

    testing window movement

--- dev.sh
+++ dev.sh
@@ -4,4 +4,9 @@ cd ~/work/Audio-Redirector || exit 1
 
 kitty --detach nvim ~/work/Audio-redirector >/dev/null
 
+# Ordering desktop
+# bspc node -d 0
+# bspc desktop -f DP-4
+
+# Starting dev env
 nix-shell shell.nix
