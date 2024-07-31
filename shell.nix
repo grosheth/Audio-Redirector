25873906ebff7c03e529d8d5daeb7d61a806dec6
salledelavage <salledelavager@gmail.com>
2024-07-31 08:02:54 -0400

    requirements

--- shell.nix
+++ shell.nix
@@ -3,9 +3,7 @@ let
 in pkgs.mkShell {
   packages = [
     (pkgs.python311.withPackages (python-pkgs: [
-      # python-pkgs.ffmpeg-python
       python-pkgs.pyaudio
-      # python-pkgs.wave
     ]))
   ];
 }
